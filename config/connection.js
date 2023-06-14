7b410758d130eddfd70d1c676352593a549b98f3
blackshane <122633142+blackshane@users.noreply.github.com>
2023-06-13 22:26:41 -0500

    Started models, set up index.js for models.

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 const { connect, connection } = require('mongoose');
 
 // TODO: mongodb connection goes here
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/FILL-IN-HERE!';
+
+connect(connectionString);
+
 
 module.exports = connection;
---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type:String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'email Address not Valid'],
+        },
+        thoughts: {
+            
+        }
+    }
+)
