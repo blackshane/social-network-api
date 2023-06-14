67626ebe5040806a8140e676969355094e946dd3
blackshane <122633142+blackshane@users.noreply.github.com>
2023-06-14 15:28:17 -0500

    Routes added, models updated, index.js files for api set up

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,64 @@
 const { Schema, model } = require('mongoose'); 
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// Virtual property to get the reactionCount
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,65 @@
+// Import required modules and models
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// GET route for retrieving all users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while retrieving users' });
+  }
+});
+
+// GET route for retrieving a specific user by ID
+router.get('/users/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while retrieving the user' });
+  }
+});
+
+// POST route for creating a new user
+router.post('/users', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to create the user' });
+  }
+});
+
+// PUT route for updating a user
+router.put('/users/:userId', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to update the user' });
+  }
+});
+
+// DELETE route for deleting a user
+router.delete('/users/:userId', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to delete the user' });
+}
+});
+
+module.exports = router;
